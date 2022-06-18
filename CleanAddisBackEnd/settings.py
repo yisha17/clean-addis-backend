c7bad6acadee3b0b0bf28a608038e3e4a54f805d
yisha17 <it.yisak.wondim@gmail.com>
2022-06-18 23:25:23 +0300

    database solved

--- CleanAddisBackEnd/settings.py
+++ CleanAddisBackEnd/settings.py
@@ -86,16 +86,16 @@ ASGI_APPLICATION = 'CleanAddisBackEnd.asgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-if DEBUG:
+# if DEBUG:
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:    
-    DATABASES = {
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:    
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'cleanaddis',
