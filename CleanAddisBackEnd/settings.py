490f3b72671ae338c40545e3f8918639715f5f7b
yisha17 <it.yisak.wondim@gmail.com>
2022-06-18 22:06:34 +0300

    deploying app

--- CleanAddisBackEnd/settings.py
+++ CleanAddisBackEnd/settings.py
@@ -87,8 +87,12 @@ ASGI_APPLICATION = 'CleanAddisBackEnd.asgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'cleanaddis',
+        'USER': 'postgres',
+        'PASSWORD': 'test123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
